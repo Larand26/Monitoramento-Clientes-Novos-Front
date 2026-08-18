7be04548224b9c3000aba9c5751390863e2d82dd
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-18 16:20:00 -0300

    style: cria o componente NavBar e adiciona ele na página Home

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,35 @@
+import isoLogo from "../assets/logo-isologo_white.svg";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className="bg-card py-2 px-4 flex justify-between items-center">
+        <div>
+          <img src={isoLogo} alt="Logo" className="w-20" />
+        </div>
+        <div>
+          <ul className="flex gap-5">
+            <li>
+              <a href="" className="text-main hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-main hover:text-gray-300">
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-main hover:text-gray-300">
+                Clientes
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="user"></div>
+        </div>
+      </nav>
+    </>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar";
+
 export function Home() {
   return (
-    <div className="p-8">
+    <div>
+      <NavBar />
       <h1 className="text-2xl font-bold text-slate-800">Início</h1>
       <p className="text-slate-600 mt-2">
         Bem-vindo ao sistema de monitoramento.
