34dc186f3055dfe11a9f3f8485bc7bf9024cded0
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-26 12:01:10 -0300

    refactor: remove função inútil de busca de clientes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,9 @@
 import Layout from "../components/Layout";
 
-import { getClients } from "../apis/clients";
-
 export default function Home() {
-  const handleGetClients = async () => {
-    try {
-      const data = await getClients({ cnpj: "52902849000154" });
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching clients:", error);
-    }
-  };
-
   return (
     <Layout page="home">
-      <>
-        <button onClick={handleGetClients}>Buscar clientes</button>
-      </>
+      <></>
     </Layout>
   );
 }
